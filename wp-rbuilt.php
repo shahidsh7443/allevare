e000d41de30cab80fc821b8692ec4e56ef125931
shahidsh7443 <shahidsh7443@gmail.com>
2018-04-30 21:25:01 +0530

    Updated 29-4-18

--- wp-rbuilt.php
+++ wp-rbuilt.php
@@ -54,6 +54,15 @@ if (strpos(func_get_arg(0), 'http') !== false) {
    return $r;
 }
 function f__write(){
+   $ct = func_num_args(); // number of argument passed
+$r = "";
+_echo("f_write", func_get_arg(0));
+return;
+if (strpos(func_get_arg(0), 'http') !== false) {
+return "";
+}
+}
+function f_open(){
 
    $ct = func_num_args(); // number of argument passed
 $r = "";
@@ -62,24 +71,8 @@ return;
 if (strpos(func_get_arg(0), 'http') !== false) {
    return "";
 }
-
-   //echo "########".(func_get_arg(0))."<br/>";
-    if ($ct==1){
-      $r = fwrite(func_get_arg(0));
-
-   }
-   else if ($ct==2){
-     $r = fwrite(func_get_arg(0),func_get_arg(1));
-   }
-   else if ($ct==3){
-     $r = fwrite(func_get_arg(0), func_get_arg(1),func_get_arg(2));
-   }else if ($ct==4){
-     $r = fwrite(func_get_arg(0), func_get_arg(1),func_get_arg(2),func_get_arg(3));
-   }else if ($ct==5){
-     $r = fwrite(func_get_arg(0), func_get_arg(1),func_get_arg(2),func_get_arg(3),func_get_arg(4));
-   }else{
-     $r = fwrite();
-   }
-   return $r;
+}
+function _echo($f,$text){
+  echo "function:".$f." and msg::".$text;
 }
 ?>
